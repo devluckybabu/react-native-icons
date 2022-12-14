6cf5f0cc6f4ab900584cc72f66f24bb4339589da
devluckybabu <dev.luckybabu@gmail.com>
2022-12-14 09:12:00 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,33 @@
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Foundation from 'react-native-vector-icons/Foundation';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Octicons from 'react-native-vector-icons/Octicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Zocial from 'react-native-vector-icons/Zocial';
+
+const Icons = {
+  MaterialCommunityIcons,
+  SimpleLineIcons,
+  SimpleLineIcons,
+  MaterialIcons,
+  FontAwesome5,
+  FontAwesome,
+  Foundation,
+  AntDesign,
+  EvilIcons,
+  Fontisto,
+  Ionicons,
+  Octicons,
+  Feather,
+  Entypo,
+  Zocial
+};
+export default Icons;
